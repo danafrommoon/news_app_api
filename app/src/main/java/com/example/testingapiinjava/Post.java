4c3fce5073a1f906f2cb530f58d7042a7299c9d2
aidana <a.orunbassarova@astanait.edu.kz>
2021-07-31 10:09:34 +0500

    news api

---
+++ app/src/main/java/com/example/testingapiinjava/Post.java
@@ -0,0 +1,29 @@
+package com.example.testingapiinjava;
+
+
+public class Post {
+
+    private String userId , id , title , body;
+
+    public Post(String userId, String title, String body) {
+        this.userId = userId;
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
